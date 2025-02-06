8ab49793b640b875f8d3c3023b58f6de2eef83cb
kid <venmanassery.vivek@gmail.com>
2025-02-07 04:32:08 +0530

    Frontend done upto sending data for resend otp, maximum times of otp resend not yet working. Will work on that tomorrow.

--- MemInc/authentication/authentication_views.py
+++ MemInc/authentication/authentication_views.py
@@ -22,10 +22,11 @@ class RegisterCustomer(APIView):
         cache_key = f"registration_{registration_data['email']}"
         cache_data = {
             'registration_data': registration_data,
-            'otp': otp
+            'otp': otp,
+            'attempts': 1
         }
 
-        cache.set(cache_key,cache_data, timeout=60)
+        cache.set(cache_key,cache_data, timeout=120)
 
         send_mail(
             'Verify your Email',
@@ -38,9 +39,55 @@ class RegisterCustomer(APIView):
 
         return Response({
             "message": "Please check your registered email for otp.",
-            "email": registration_data['email']
-        }, status=status.HTTP_201_CREATED)
+            "email": registration_data['email'],
+            'resend_attempts_remaining': 2
+        }, status=status.HTTP_200_OK)
         
+
+class ResendOtp(APIView):
+    def post(self, request):
+        email = request.data['email']
+        cache_key = f'registration_{email}'
+        cache_data = cache.get(cache_key)
+        if not cache_data:
+            return Response({'error': 'Registration session expired.Try again','registration_timeout': True}, status=status.HTTP_400_BAD_REQUEST)
+        attempts = cache_data['attempts']
+        if attempts > 3:
+            return Response({
+                'error':'Maximum otp resend attempts reached. Please start new registration',
+                'registration_timeout': True
+            }, status= status.HTTP_429_TOO_MANY_REQUESTS)
+        
+        new_otp = ''.join(str(random.randint(0,9)) for i in range(0,6))
+        registration_data = cache_data['registration_data']
+        new_cache_data = {
+            'registration_data': registration_data,
+            'otp': new_otp,
+            'attempts': attempts + 1
+        }
+        cache.set(cache_key, new_cache_data, timeout=120)
+
+        send_mail(
+            'Verify your Email',
+            f"Your otp for registration into MemInc:Fresh to home is: {new_otp}. The otp is valid for 1 minute.",
+            'meminccorporation@gmail.com',
+            [email],
+            fail_silently=False,
+        )
+        print(f"resend_otp: {new_otp}, resend attempts remaining: {3 - attempts}")
+        return Response({
+            "message": "Please check your registered email for otp.",
+            'registered_email': email,
+            'resend_attempts_remaining': 3-attempts
+        }, status=status.HTTP_200_OK)
+        
+        
+
+class CustomerOtpValidation(APIView):
+    def post(self, request):
+        data = request.data
+        print(data)
+        return Response({"message":"checking for data."}, status=status.HTTP_201_CREATED)
 
 
 
--- MemInc/authentication/urls.py
+++ MemInc/authentication/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .authentication_views import *
 
 urlpatterns = [
-    path('customer/', RegisterCustomer.as_view())
+    path('customer/', RegisterCustomer.as_view()),
+    path('customer/verifyotp/', CustomerOtpValidation.as_view()),
+    path('customer/resendotp/', ResendOtp.as_view())
 ]
 
