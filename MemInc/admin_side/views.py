b14bdf5d8ea2e2dffc0cb6278f998881aaf95f16
kid <venmanassery.vivek@gmail.com>
2025-02-10 22:52:06 +0530

    set the total_page count in list_customer and vendor response

--- MemInc/admin_side/views.py
+++ MemInc/admin_side/views.py
@@ -2,13 +2,16 @@ from rest_framework.decorators import api_view
 from authentication.models import Customer, Vendor
 from rest_framework.pagination import PageNumberPagination 
 from rest_framework.response import Response
+import math
 
 class custom_pagination(PageNumberPagination):
     page_size = 10
 
     def get_paginated_response(self, data):
+        total_pages = math.ceil(self.page.paginator.count/self.page_size)
         return Response({
             'count':self.page.paginator.count,
+            'total_pages': total_pages,
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
             'results': data
