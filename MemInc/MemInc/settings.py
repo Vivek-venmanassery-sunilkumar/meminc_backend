f6750d51f168eb25278abceca1bdcb86ec068598
kid <venmanassery.vivek@gmail.com>
2025-04-07 20:17:44 +0530

    cors multiple headers fix added

--- MemInc/MemInc/settings.py
+++ MemInc/MemInc/settings.py
@@ -160,7 +160,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authentication.CustomUser'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", "http://127.0.0.1:5173", 'https://www.meminc.store'
